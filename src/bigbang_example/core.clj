02569618763c6f344be629b2d62946affc9447c6
tangrammer <juanantonioruz@gmail.com>
2014-12-12 14:58:56 +0100

    Example finished

--- src/bigbang_example/core.clj
+++ src/bigbang_example/core.clj
@@ -6,27 +6,69 @@
             [milesian.aop.utils  :refer (logging-function-invocation)]
             [milesian.identity :as identity]
             [defrecord-wrapper.aop :refer (new-simple-protocol-matcher)]
-            [tangrammer.component.co-dependency :as co-dep]
-            [milesian.system-examples :refer (new-system-map listening talking Listen Talk)]))
+            [tangrammer.component.co-dependency :as co-dependency]
+            [milesian.system-examples :refer (new-system-map listening talking Listen Talk)])
+  (:import [clojure.lang Atom]))
 
 
-(defn ^{:bigbang/phase :after-start} assoc-atom-system
-  [c* system]
-  (assert (not (nil? (:bigbang/key (meta c*))))
-          "this fn needs your components meta tag with :api-component/key")
-  (swap! system assoc (:bigbang/key (meta c*)) c*)
-  c*
-)
 
 (def system-map (new-system-map))
 
 
-(let [system-atom (atom system-map)]
-  (-> (bigbang/expand system-map
-                     {:before-start [[identity/add-meta-key system-map]
-                                     [co-dep/assoc-co-dependencies system-atom]
-                                     [identity/assoc-meta-who-to-deps]]
-                      :after-start  [[assoc-atom-system system-atom]
-                                     [aop/wrap logging-function-invocation]]})
-     :c
-     talking))
+
+(let [started-system (bigbang/expand system-map
+                                     {:before-start [[identity/add-meta-key system-map]]
+                                      :after-start  []})]
+  (assert (= :c (-> started-system :c meta :bigbang/key)))
+  )
+
+
+(let [started-system (bigbang/expand system-map
+                                     {:before-start [[identity/add-meta-key system-map]
+                                                     [identity/assoc-meta-who-to-deps]]
+                                      :after-start  []})]
+  (assert (= :c (-> started-system :c meta :bigbang/key)))
+  (assert (= :c (-> started-system :c :b meta :bigbang/who)))
+  )
+
+
+
+
+(let [started-system (bigbang/expand system-map
+                                     {:before-start [[identity/add-meta-key system-map]
+                                                     [identity/assoc-meta-who-to-deps]]
+                                      :after-start  [[aop/wrap logging-function-invocation]]})]
+
+  (assert (= :c (-> started-system :c meta :bigbang/key)))
+  (assert (= :c (-> started-system :c :b meta :bigbang/who)))
+  (-> started-system :c talking)
+  ;; check the repl to see the logging function invocation
+  ;; REPL->c: talking
+  ;; c->b: listening
+
+  )
+
+
+
+
+
+
+(let [system-map  (assoc system-map :b  (-> (:b system-map)
+                                            (co-dependency/co-using [:c])))
+      system-atom (atom system-map)
+      started-system (bigbang/expand system-map
+                      {:before-start [[identity/add-meta-key system-map]
+                                      [co-dependency/assoc-co-dependencies system-atom]
+                                      [identity/assoc-meta-who-to-deps]]
+                       :after-start  [[co-dependency/update-atom-system system-atom]
+                                      [aop/wrap logging-function-invocation]]})]
+  (assert (= :c (-> started-system :c meta :bigbang/key)))
+  (assert (= :c (-> started-system :c :b meta :bigbang/who)))
+  (-> started-system :c talking)
+    ;; check the repl to see the logging function invocation
+  ;; REPL->c: talking
+  ;; c->b: listening
+
+  (assert (= (-> started-system :c :state) (-> started-system :b :c deref :state)))
+
+  )
